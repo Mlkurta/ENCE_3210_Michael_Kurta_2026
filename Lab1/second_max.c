baa3a1385066146df562f568880b0039b586177f
Mlkurta <78199728+Mlkurta@users.noreply.github.com>
2026-01-11 14:33:59 -0700

    Refactor second_max.c with comments and formatting
    Added comments for clarity and improved code readability.

--- Lab1/second_max.c
+++ Lab1/second_max.c
@@ -4,26 +4,31 @@
 
 // Global variables
 
+// Note that this is a straight C program; it's not formatted for use with Arduino, but can be easily adapted to do so.
+
+// initialize a zero array of size = ARRAY_SIZE
 int gMyArray[ARRAY_SIZE] = {0};
 
+// initialized integers to store the max and second-max numbers found
 int gMax;
 int g2Max;
 
 
 int main()
 {   
-
+    // Fill the array with known maximum and second maximum numbers. 
     for(int i = 0; i < ARRAY_SIZE; i++){
         gMyArray[i] = 50 - i;
     }
 
+    // Loop through the array
     for(int i = 0; i < ARRAY_SIZE - 1; i++){
-        if(gMyArray[i] > gMax){
-          g2Max = gMax;
-          gMax = gMyArray[i];
+        if(gMyArray[i] > gMax){             // If the current value is greater than the highest,
+          g2Max = gMax;                     // save that number as the new second-highest.
+          gMax = gMyArray[i];               // Then save the highest number variable as the current number
     
-        }else if (gMyArray[i] > g2Max){
-          g2Max = gMyArray[i];
+        }else if (gMyArray[i] > g2Max){     // Otherwise if the current number is ONLY larger than the second highest,
+          g2Max = gMyArray[i];              // save the second highest as the current number
         }
     }
     
@@ -37,5 +42,6 @@ int main()
         printf("\n2nd maximum: %d", g2Max);
 
     return 0;
+
 
 }
