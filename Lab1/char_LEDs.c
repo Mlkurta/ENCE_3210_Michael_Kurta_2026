c8815eab2ee078880cc100368a3584a323feb47e
Mlkurta <78199728+Mlkurta@users.noreply.github.com>
2026-01-11 14:14:51 -0700

    Enhance color detection with LED control
    Added pin definitions and LED control for color detection.

--- Lab1/char_LEDs.c
+++ Lab1/char_LEDs.c
@@ -1,7 +1,13 @@
 #define ARRAY_SIZE 14
 
+#define RED_PIN 11
+#define GREEN_PIN 10
+#define BLUE_PIN 9
+
+// Enter your desired word in curly brackets, and update ARRAY_SIZE macro -> number of characters + 1 (for '\0')
 char gMyArray[ARRAY_SIZE] = {"biogeographer"};
 
+// Initialize global struct for color counts
 struct colorCount{
   uint8_t RED = 0;
   uint8_t GREEN = 0;
@@ -11,41 +17,61 @@ struct colorCount{
 struct colorCount gC;
 
 void foundRed(){
+  // Increment counter
   gC.RED += 1;
+  // Print color / count
   Serial.print("RED \tCount: ");
   Serial.println(gC.RED);
-  // Turn on LED pin
+  // Turn on Red LED
+  digitalWrite(RED_PIN, HIGH);
+  // Delay 0.5 sec
   delay(500);
-  // Turn off LED pin
+  // Turn off Red LED
+  digitalWrite(RED_PIN, LOW);
 
 }
 
 void foundGreen(){
+  // Increment counter
   gC.GREEN += 1;
+  // Print color / count
   Serial.print("GREEN \tCount: ");
   Serial.println(gC.GREEN);
-  // Turn on LED pin
+  // Turn on Green LED
+  digitalWrite(GREEN_PIN, HIGH);
+  // Delay 0.5 sec
   delay(500);
-  // Turn off LED pin
+  // Turn off Green LED 
+  digitalWrite(GREEN_PIN, LOW);
 
 }
 
 void foundBlue(){
+  // Increment counter
   gC.BLUE += 1;
+  // Print color / count
   Serial.print("BLUE \tCount: ");
   Serial.println(gC.BLUE);
-  // Turn on LED pin
+  // Turn on Blue LED
+  digitalWrite(BLUE_PIN, HIGH);
+  // Delay 0.5 sec
   delay(500);
-  // Turn off LED pin
+  // Turn off Blue LED
+  digitalWrite(BLUE_PIN, LOW);
 
 }
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
 
   int i;
   Serial.println();
+
+  // Loop through char array for key letters (r,R,g,G,b,B) and call the appropriate function.
   for(i = 0; i < ARRAY_SIZE - 1; i++){
     
     if((gMyArray[i] == 'r') || (gMyArray[i] == 'R')){
